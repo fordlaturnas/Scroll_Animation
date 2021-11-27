586ca2a4e1ebe68cabba0687544d3e4820c6f077
Ford Laturnas <fordlaturnas1209@gmail.com>
2021-11-28 00:03:01 +0800

    fix-responsive

--- script.js
+++ script.js
@@ -4,18 +4,16 @@ window.addEventListener('scroll', checkboxes);
 
 checkboxes();
 
-function checkboxes () {
-    const triggerBottom = window.innerHeight / 5 * 4;
+function checkboxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if (boxTop < triggerBottom) {
-            box.classList.add('show');
-        } else {
-            box.classList.remove('show');
-        }
-
-    })
-
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
